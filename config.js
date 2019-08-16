271212ad5cd82b460711daba006b2a57c535a3f8
myst42 <39014918+myst42@users.noreply.github.com>
2019-08-16 18:26:12 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,52 @@
+var CONFIG = {
+  "maps": {
+    "map_myworld": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1565972946,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 5,
+      "name": "My World",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "myworld_isometric_t1",
+      "tileSize": 384,
+      "world": "myworld",
+      "worldName": "myworld",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "map_myworld"
+  ],
+  "tileSetGroups": {
+    "myworld_isometric_t1": {
+      "maxZoom": 5,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
